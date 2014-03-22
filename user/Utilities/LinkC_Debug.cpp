714fc3ec62fcf513a48f0178b419edf0810d2523
Trickness <564691478@qq.com>
2014-03-22 15:58:12 +0800

    写了半天没写出来[汗]

--- user/Utilities/LinkC_Debug.cpp
+++ user/Utilities/LinkC_Debug.cpp
@@ -1,8 +1,31 @@
+/*
+ * Author		： Junfeng Zhang <564691478@qq.com>
+ * Last-Change		： March 22, 2014
+ */
 #include "LinkC_Utilities.h"
 #include <stdio.h>
+#include <string.h>
 
 void LinkC_Debug(const char *DebugMessage){
 #if _DEBUG_MOD_
     printf("Debug >> %s\n",DebugMessage);
 #endif
 }
+/*
+void LinkC_Debug(const char *Action,const char *Status){
+    void *DebugMessage;
+    int len = strlen(Action);
+    int i;
+    if(len < _DEFAULT_ACTION_LENGTH){
+        DebugMessage = new char[_DEFAULT_ACTION_LENGTH + 4 + strlen(Status)];
+        strcpy((char*)DebugMessage,Action);
+        for(i=0;i<_DEFAULT_ACTION_LENGTH - len;i++){
+            (char *)DebugMessage+(len+i) = ' ';
+        }
+        (char *)DebugMessage+(i+len) = '=';
+        strcpy((char *)DebugMessage+(i+len+1),Status);
+        (char *)DebugMessage+(i+len+1+strlen(Status)+1) = 0;
+    }
+
+}
+*/
