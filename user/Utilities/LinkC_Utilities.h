263e99ea76176fb54ae797b14304f76dd108a71f
Trickness <564691478@qq.com>
2014-03-22 15:58:39 +0800

    。。。。

--- user/Utilities/LinkC_Utilities.h
+++ user/Utilities/LinkC_Utilities.h
@@ -1,8 +1,14 @@
+/*
+ * Author		： Junfeng Zhang <564691478@qq.com>
+ * Last-Change		： March 22, 2014
+ */
 #ifndef _LINKC_UTILITIES_
 #define _LINKC_UTILITIES_
 
-#define _DEBUG_MOD_ 1
+#define _DEBUG_MOD_                 1
+#define _DEFAULT_ACTION_LENGTH      35
 
 void LinkC_Debug(const char *DebugMessage);
+void LinkC_Debug(const char *Action,const char *Status);
 
 #endif
