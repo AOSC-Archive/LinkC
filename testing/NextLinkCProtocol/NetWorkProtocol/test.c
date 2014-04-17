875a5b10ad8004c87366ed52e45a38bc4a714f23
Trickness <564691478@qq.com>
2014-04-17 22:32:50 +0800

    添加了一部分注释

--- testing/NextLinkCProtocol/NetWorkProtocol/test.c
+++ testing/NextLinkCProtocol/NetWorkProtocol/test.c
@@ -4,13 +4,13 @@
 
 int main(){
     struct sigaction Act;                   //  定义处理信号的参数集合
-    sigemptyset(&Act.sa_mask);              //  设置掩码[不明白]
+    sigemptyset(&Act.sa_mask);              //  将数据清空
     Act.sa_sigaction=TimerInt;              //  设置回调函数[请在LinkC_NetWorkProtocol.c中查找]
-    Act.sa_flags=SA_SIGINFO;                //  设置Flag[不明白]
+    Act.sa_flags=SA_SIGINFO;                //  使用sa_sigaction参数的函数最为信号发来后的处理函数[也就是上面定义的]
                     
     if(sigaction(SIGALRM,&Act,NULL)==-1)    //  安装信号
         printf("install error~!\n");        //      参数[1]-->  信号，这里是时钟信号[后面的第三个参数我就不清楚了]
     alarm(1);                               //  1秒后发射信号
-    while(1){};
+    while(1){sleep(2);};
     return 0;
 }
