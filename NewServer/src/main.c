fd9afcc6e6dd0fbc0bee64729c51b0081ade9ba7
wiiiky <wiiiky@yeah.net>
2014-07-15 22:21:28 +0800

    将服务器启动为守护进程，并将所有输出重定向到/tmp/linkc_server.log

--- NewServer/src/main.c
+++ NewServer/src/main.c
@@ -3,22 +3,111 @@
 #include "../include/linkc_db.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <signal.h>
+
+/*
+ * 初始化为守护进程
+ *
+ * 《UNIX环境高级编程》中文第三版375页
+ *
+ * 略加修改
+ */
+static void daemonize(const char *name);
 
 int main()
 {
-    pid_t LCServer = 0;             // LinkC Server 主进程
-    if ((LCServer = fork()) < -1){  //  如果创建新进程失败
-        perror("fork");             //      打印错误消息
-        exit (EXIT_FAILURE);        //      退出
-    }
-    else if (LCServer == 0){        //  如果是子进程[即LinkC Server 主进程]
-        if(InitNetwork(2341) < 0){
-            LinkC_Debug("Init Network",LINKC_FAILURE);
-            return -1;
-        }
-        WaitForConnect();
-    }
-    return 0;                       //  返回
+	daemonize("LinkC Server");
+	WaitForConnect();
+	return 0;                       //  返回
 }
+
+
+
+#define ERROR(err)	do{\
+	perror(err);\
+	exit(EXIT_FAILURE);\
+}while(0);
+static void daemonize(const char *name)
+{
+	int i,fd0,fd1,fd2;
+	pid_t pid;
+	struct rlimit rl;
+	struct sigaction sa;
+
+	printf("Initializing %s...\n",name);
+
+	/*
+	 * Clear file create mask
+	 */
+	umask(0);
+
+	/*
+	 * Get the maximum number of file descriptor
+	 */
+	if(getrlimit(RLIMIT_NOFILE,&rl)<0){
+		ERROR("Fail to get file limit");
+	}
+
+	/*
+	 * Become a session leader to lose controlling TTY.
+	 */
+	if((pid=fork())<0){
+		ERROR("Can't fork");
+	}else if(pid!=0){
+		exit(0);	/* parent */
+	}
+	setsid();
+
+	/*
+	 * Ensure future opens won't allocate controlling TTYs.
+	 */
+	sa.sa_handler=SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags=0;
+	if(sigaction(SIGHUP,&sa,NULL)<0){
+		ERROR("Can't ignore SIGHUP");
+	}
+	if((pid=fork())<0){
+		ERROR("Can't fork");
+	}else if(pid!=0){
+		exit(0);
+	}
+
+	/*
+	 * Change the current working directory to the root so
+	 * we won't prevent file systems from being umounted
+	 */
+	if(chdir("/")<0){
+		ERROR("Fail to change directory to /");
+	}
+
+	printf("%s started\n",name);
+
+	/*
+	 * Close all open file descriptors
+	 */
+	if(rl.rlim_max==RLIM_INFINITY){
+		rl.rlim_max=1024;
+	}
+	for(i=0;i<rl.rlim_max;i++){
+		close(i);
+	}
+
+	/*
+	 * Attach file descriptors 0,1 and 2 to /dev/null
+	 *
+	 * 因为所有文件描述符都已经被关闭，所以fd0=0,fd1=1,fd2=2
+	 */
+	fd0=open("/tmp/linkc_server.log",O_RDWR|O_CREAT|O_TRUNC,0666);
+	fd1=dup(0);
+	fd2=dup(0);
+	if(fd0!=0 || fd1!=1 || fd2!=2){
+		exit(-1);
+	}
+}
+#undef ERROR
