afdcb9390cc742d0ed5fff7ccdf53c54707aec74
trickness <564691478@qq.com>
2014-07-15 22:46:22 +0800

    合并代码

--- NewServer/src/error/linkc_error.c
+++ NewServer/src/error/linkc_error.c
@@ -4,49 +4,49 @@
 
 void LinkC_Debug(const char *Target,int Status){
 #if _DEBUG_MOD_
-    if(Status == LINKC_SUCCESS)
+	if(Status == LINKC_SUCCESS)
 #ifdef linux
-        printf("[\033[32mSUCCESS\033[0m]\t%s\n",Target);
+	   printf("[\033[32mSUCCESS\033[0m]\t%s\n",Target);
 #else
-        printf("[SUCCESS]\t%s\n",Target);
+	printf("[SUCCESS]\t%s\n",Target);
 #endif
-    else if(Status == LINKC_EXITED)
+	else if(Status == LINKC_EXITED)
 #ifdef linux
-        printf("[\033[33mEXITED \033[0m]\t%s\n",Target);
+	   printf("[\033[33mEXITED \033[0m]\t%s\n",Target);
 #else
-        printf("[EXITED ]\t%s\n",Target);
+	printf("[EXITED ]\t%s\n",Target);
 #endif
-    else if(Status == LINKC_WARNING)
+	else if(Status == LINKC_WARNING)
 #ifdef linux
-        printf("[\033[34mWARNING\033[0m]\t%s\n",Target);
+	   printf("[\033[34mWARNING\033[0m]\t%s\n",Target);
 #else
-        printf("[WARNING]\t%s\n",Target);
+	printf("[WARNING]\t%s\n",Target);
 #endif
-    else if(Status == LINKC_DONE)
+	else if(Status == LINKC_DONE)
 #ifdef linux
-        printf("[\033[32mDONE   \033[0m]\t%s\n",Target);
+	   printf("[\033[32mDONE \033[0m]\t%s\n",Target);
 #else
-        printf("[DONE   ]\t%s\n",Target);
+	printf("[DONE ]\t%s\n",Target);
 #endif
-    else if(Status == LINKC_STARTED)
+	else if(Status == LINKC_STARTED)
 #ifdef linux
-        printf("[\033[32mSTARTED\033[0m]\t%s\n",Target);
+	   printf("[\033[32mSTARTED\033[0m]\t%s\n",Target);
 #else
-        printf("[STARTED]\t%s\n",Target);
+	printf("[STARTED]\t%s\n",Target);
 #endif
-    else if(Status == LINKC_DEBUG)
+	else if(Status == LINKC_DEBUG)
 #ifdef linux
-        printf("[\033[34mDEBUG\033[0m  ]\t%s\n",Target);
+	    printf("[\033[34mDEBUG\033[0m]\t%s\n",Target);
 #else
-        printf("[DEBUG  ]\t%s\n",Target);
+	printf("[DEBUG ]\t%s\n",Target);
 #endif
 #endif
 
-    if(Status == LINKC_FAILURE)
+	if(Status == LINKC_FAILURE)
 #ifdef linux
-            printf("[\033[31mFAILURE\033[0m]\t%s\n",Target);
+	   printf("[\033[31mFAILURE\033[0m]\t%s\n",Target);
 #else
-            printf("[FAILURE]\t%s\n",Target);
+	printf("[FAILURE]\t%s\n",Target);
 #endif
 }
 
